bb30d54423c613147d2e849aa8ef03f73b8eb461
Sneha P Pratap <snehapratap248@gmail.com>
2025-06-17 22:30:25 +0530

    done

---
+++ detector/yolo_detector.py
@@ -0,0 +1,15 @@
+from ultralytics import YOLO
+import cv2
+
+class YOLODetector:
+    def __init__(self, model_path='models/yolov8n-face.pt'):
+        self.model = YOLO(model_path)
+
+    def detect_faces(self, image):
+        results = self.model(image)
+        bboxes = []
+        for r in results:
+            for box in r.boxes:
+                x1, y1, x2, y2 = map(int, box.xyxy[0])
+                bboxes.append((x1, y1, x2, y2))
+        return bboxes
